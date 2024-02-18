e3aeb5f1dff914df93f508e5808d2b6fae8c6110
Lyndon Purcell <ljpurcell.dev@gmail.com>
2024-02-19 08:56:22 +1100

    add initial pdf reader functionality

--- main.go
+++ main.go
@@ -2,15 +2,74 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/unidoc/unipdf/v3/extractor"
+	"github.com/unidoc/unipdf/v3/model"
 )
 
 func main() {
+    file := ""
+
+    if err := outputPdfText(file); err != nil {
+        fmt.Printf("Could not output PDF: %v", err)
+    }
+}
+
+func outputPdfText(inputPath string) error {
+	f, err := os.Open(inputPath)
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	pdfReader, err := model.NewPdfReader(f)
+	if err != nil {
+		return err
+	}
+
+	numPages, err := pdfReader.GetNumPages()
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("--------------------\n")
+	fmt.Printf("PDF to text extraction:\n")
+	fmt.Printf("--------------------\n")
+	for i := 0; i < numPages; i++ {
+		pageNum := i + 1
+
+		page, err := pdfReader.GetPage(pageNum)
+		if err != nil {
+			return err
+		}
+
+		ex, err := extractor.New(page)
+		if err != nil {
+			return err
+		}
+
+		text, err := ex.ExtractText()
+		if err != nil {
+			return err
+		}
+
+		fmt.Println("------------------------------")
+		fmt.Printf("Page %d:\n", pageNum)
+		fmt.Printf("\"%s\"\n", text)
+		fmt.Println("------------------------------")
+	}
+
+	return nil
+}
+
+func postToAPI() {
 
 	url := "https://api.openai.com/v1/audio/speech"
 
